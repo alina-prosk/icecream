523d3bfa9aa9fe8e4588748746eca613d6feda67
alina-prosk <100199512+alina-prosk@users.noreply.github.com>
2022-06-14 05:15:02 +0000

    Deploying to gh-pages from @ alina-prosk/icecream@21b2592484ef89dc4a1b751a64d59d32cdab8c0c 🚀

---
+++ index.1cc03e19.js
@@ -0,0 +1,2 @@
+$(".slider__wrapper").slick({arrows:!1,dots:!0,infinite:!0,speed:700,swipe:!0,cssEase:"linear",centerMode:!1,variableWidth:!1});
+//# sourceMappingURL=index.1cc03e19.js.map
--- index.50821313.js
+++
@@ -1,2 +0,0 @@
-$(".slider__wrapper").slick({arrows:!1,dots:!0,infinite:!0,speed:1e3,swipe:!0,cssEase:"linear",centerMode:!1,variableWidth:!1});
-//# sourceMappingURL=index.50821313.js.map
